d376248f6202722cc2642b68c0c674b47d0d1acd
mattpurpura <mpurpura212@gmail.com>
2018-08-27 18:14:40 -0700

    still need to display initial health

--- assets/javascript/rpg.js
+++ assets/javascript/rpg.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
- var players = [
+var players;
+
+function initialSetup(){
+  players = [
      
     {   name: "Mario",
         health: 100,
@@ -9,14 +12,14 @@ $(document).ready(function() {
         counter: 20,
     },  
     {
-        name: "luigi",
+        name: "Luigi",
         health: 150,
         attack: 5,
         increment: 5,
         counter: 5,
     },
     {
-        name: "wario",
+        name: "Princess Peach",
         health: 150,
         attack: 8,
         increment: 8,
@@ -24,7 +27,7 @@ $(document).ready(function() {
         
     },
     {
-        name: "waluigi",
+        name: "Bowser",
         health: 150,
         attack: 3,
         increment: 3,
@@ -32,8 +35,8 @@ $(document).ready(function() {
         
     }
  ];
-
-
+}
+initialSetup();
 
     // console.log(obiwan.health);
  var playerone = players[0];
@@ -47,7 +50,7 @@ $(document).ready(function() {
 
 
  function heroSelect(){
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 4; i++){
       if ($("#char" + i).hasClass("hero")){
         playerone = players[i];
         $("#player-one").prepend($(".hero"));
@@ -60,7 +63,7 @@ $(document).ready(function() {
  }
 
  function defenderSelect(){
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 4; i++){
         if ($("#char" + i).hasClass("defender") && players[i].health > 0){
           playertwo = players[i];
           $("#defender-row").prepend($(".defender"));
@@ -74,22 +77,33 @@ $(document).ready(function() {
  }
 
  function removeDefender(){
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 4; i++){
         if ($("#char" + i).hasClass("defender")){
             $("#char" + i).removeClass("defender")
-            $("#char"+ i).addClass("enemy")
+            $("#char"+ i).addClass("enemy defeated")
             $("#enemy-select").append($(".enemy"))
        } 
       }
  }
 
  function attack(){
+     $("#attack-message").html(playerone.name + " attacked " + playertwo.name + " for " + playerone.attack);
+     $("#counter-message").html(playertwo.name + " countered for " + playertwo.counter);
      playertwo.health -= playerone.attack;
      playerone.health -= playertwo.counter;
      playerone.attack += playerone.increment;
-     
+     $("#hero-health").html(playerone.health);
+
+    if(playertwo.health > 0){
+        $("#defender-health").html(playertwo.health);
+    }
+        else{
+            $("#defender-health").html("");
+        }
+
      if (playerone.health <= 0){
         alert("YOU SUCK STUPID")
+        reset();
      }
 
      if (playertwo.health <=0){
@@ -100,102 +114,78 @@ $(document).ready(function() {
      
  }
 
+function winLogic(){
+    if ($("#char0").hasClass("hero") && players[1].health <= 0 && players[2].health <= 0 && players[3].health <= 0){
+        alert("You won the game as " + playerone.name);
+        reset();
+    }
+    else if ($("#char1").hasClass("hero") && players[0].health <= 0 && players[2].health <= 0 && players[3].health <= 0){
+        alert("You won the game as " + playerone.name);
+        reset();
+    }
+    else if ($("#char2").hasClass("hero") && players[0].health <= 0 && players[1].health <= 0 && players[3].health <= 0){
+        alert("You won the game as " + playerone.name);
+        reset();
+    }
+    else if ($("#char3").hasClass("hero") && players[0].health <= 0 && players[2].health <= 0 && players[1].health <= 0){
+        alert("You won the game as " + playerone.name);
+        reset();
+    }
+}
 
-//  function defenderSelect(){
-//      $(this).addClass("defender");
-//  }
+ function reset(){
+    $("#defender-health").html("");
+    $("#hero-health").html("");
+    for(let i = 0; i < 4; i++){
+        $("#char"+i).appendTo($("#hero-select"))
+        if ($("#char" + i).hasClass("defender")){
+            $("#char" + i).removeClass("defender")
+        } 
+            else if ($("#char" + i).hasClass("enemy")){
+                $("#char" + i).removeClass("enemy")
+
+                if ($("#char" + i).hasClass("defeated")){
+                    $("#char" + i).removeClass("defeated")
+                }
+            }
+
+            else if ($("#char" + i).hasClass("hero")){
+                $("#char" + i).removeClass("hero")
+            }
+    }
+    firstChoice = true;
+    secondChoice = true;
+      initialSetup();
+      
+    }
+
 
 $(".btn").on("click", function(){
  if (firstChoice === true){
     firstChoice = false;
     $(this).addClass("hero");
     heroSelect();
+    $("#hero-health").html(playerone.health);
     console.log(playerone);
  }
     else if (secondChoice === true){
         secondChoice = false;
         $(this).addClass("defender");
         defenderSelect();
+        $("#defender-health").html(playertwo.health);
         console.log(playertwo);
     }
 
 if($(this).is("#attack")){
     attack();
     console.log("P1 Health: " + playerone.health, "P2 Health: " + playertwo.health);
+    winLogic();
 }
-    
-});
 
+if($(this).is("#reset")){
+    reset();
+}
 
+}); //closes the onCLick function
 
-
-
-
-
-
-
-
-// function appear(x){
-//     if ($(x).hasClass("invisible")){
-//       $(x).removeClass("invisible");
-//         $(x).addClass("visible");  
-//     }
-    
-// }
-
-// function disappear(x){
-//     if ($(x).hasClass("visible")){
-//       $(x).addClass("invisible");
-//         $(x).removeClass("visible");  
-//     }
-// }
-
-// $(".btn").on("click", function(){
-//     if($(this).is("#hero1")){
-//         appear(".enemy");
-//         disappear(".hero");
-//         disappear("#enemy1");
-//         appear("#hero1");
-//     }
-//     else if($(this).is("#hero2")){
-//         appear(".enemy");
-//         disappear(".hero");
-//         disappear("#enemy2");
-//         appear("#hero2");
-//     }
-//     else if($(this).is("#hero3")){
-//         appear(".enemy");
-//         disappear(".hero");
-//         disappear("#enemy3");
-//         appear("#hero3");
-//     }
-//     else if($(this).is("#hero4")){
-//         appear(".enemy");
-//         disappear(".hero");
-//         disappear("#enemy4");
-//         appear("#hero4");
-//     }
-//     else if($(this).is("#enemy1")){
-//         appear(".enemy")
-//         disappear("#enemy1");
-//         disappear(".defender");
-//         appear("#defender1");
-//     }
-//     else if($(this).is("#enemy2")){
-//         disappear("#enemy2");
-//         disappear(".defender");
-//         appear("#defender2");
-//     }
-//     else if($(this).is("#enemy3")){
-//         disappear("#enemy3");
-//         disappear(".defender");
-//         appear("#defender3");
-//     }
-//     else if($(this).is("#enemy4")){
-//         disappear("#enemy4");
-//         disappear(".defender");
-//         appear("#defender4");
-//     }
-// });
-
-});
+}); //closes the document.ready function
